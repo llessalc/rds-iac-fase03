a70c1048fc2afcdad7f2610cb86d3e59e45217dd
Guilherme L Cunha <guilhermeldcunha@gmail.com>
2024-03-16 14:01:12 +0000

    Create rule to allow ingress from multiple private subnets

--- main.tf
+++ main.tf
@@ -1,19 +1,49 @@
 ##########################################
-## VPC SETUP
+## VPC AND SUBNETS SETUP
 ##################
+# Here, we will create two new private subnets
+# inside a existing VPC and assign them to
+# the RDS instance.
 
 data "aws_subnets" "db_subnets" {
 
   filter {
-    name   = "tag:Name"
-    values = ["chosen"]
+    name = "tag:Name"
+    values = ["fiap-58-private-us-east-1a",
+    "fiap-58-private-us-east-1b"]
   }
+}
+
+data "aws_subnet" "subnet" {
+  for_each = toset(data.aws_subnets.db_subnets.ids)
+  id       = each.value
 }
 
 data "aws_vpc" "vpc" {
   filter {
     name   = "tag:Name"
-    values = ["default"]
+    values = ["fiap-58-vpc"]
+  }
+}
+
+# Subnets creation
+resource "aws_subnet" "private-a" {
+  vpc_id            = data.aws_vpc.vpc.id
+  cidr_block        = "10.0.11.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    "Name" = "private-subnet-58-a"
+  }
+}
+
+resource "aws_subnet" "private-b" {
+  vpc_id            = data.aws_vpc.vpc.id
+  cidr_block        = "10.0.9.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    "Name" = "private-subnet-58-b"
   }
 }
 
@@ -23,7 +53,7 @@ data "aws_vpc" "vpc" {
 # is deploy in the default VPC
 resource "aws_db_subnet_group" "db_subnet_group" {
   name       = "db-subnet-group-58"
-  subnet_ids = data.aws_subnets.db_subnets.ids
+  subnet_ids = [aws_subnet.private-a.id, aws_subnet.private-b.id]
 
   tags = {
     Name = "db-subnet-group"
@@ -35,12 +65,6 @@ resource "aws_db_subnet_group" "db_subnet_group" {
 ## SECURITY GROUP AND INBOUND RULES
 ##################
 
-data "aws_subnet" "ec2_sub" {
-  filter {
-    name   = "tag:Name"
-    values = ["ec2_sub"]
-  }
-}
 
 resource "aws_security_group" "allow_node_group" {
   name        = "allow_node_group"
@@ -53,8 +77,10 @@ resource "aws_security_group" "allow_node_group" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_node_group" {
+  # Allow each subnet to access the RDS instance
+  for_each          = data.aws_subnet.subnet
   security_group_id = aws_security_group.allow_node_group.id
-  cidr_ipv4         = data.aws_subnet.ec2_sub.cidr_block
+  cidr_ipv4         = each.value.cidr_block
   from_port         = 3306
   ip_protocol       = "tcp"
   to_port           = 3306
@@ -81,6 +107,8 @@ resource "aws_db_instance" "db_instance" {
   # Assign this instance to a specific VPC
   db_subnet_group_name = aws_db_subnet_group.db_subnet_group.name
 
-  # Assing group with the correct inbound rules
+  # Assign group with the correct inbound rules
   vpc_security_group_ids = [aws_security_group.allow_node_group.id]
+
+  depends_on = [aws_security_group.allow_node_group, aws_db_subnet_group.db_subnet_group]
 }
